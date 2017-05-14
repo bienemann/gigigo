9aba804a4bd0eafb49007e9e36f97903931b141e
Aya <aya@bienemann.me>
2017-05-14 19:36:12 -0300

    Implementing navigation

--- Gigigo-YouTube/API/BaseAPI.swift
+++ Gigigo-YouTube/API/BaseAPI.swift
@@ -10,13 +10,11 @@ import Foundation
 import Alamofire
 
 class BaseAPI: NSObject {
-    
-    static let ID_USER = "channelId=UCE_M8A5yxnLfW0KghEeajjw"
-    static let API_KEY = "key=AIzaSyBPlVYsLeKqj9u7wjIQFzamljTaFKPVFSE"
-    static let BASE_URL = "https://www.googleapis.com/youtube/v3/"
-    
-//    let playlistURL = "\(BaseAPI.BASE_URL)playlists?part=snippet&\(BaseAPI.ID_USER)&\(BaseAPI.API_KEY)"
-    
-    
-}
 
+    static let idUser = "channelId=UCE_M8A5yxnLfW0KghEeajjw"
+    static let apiKey = "key=AIzaSyBPlVYsLeKqj9u7wjIQFzamljTaFKPVFSE"
+    static let baseURL = "https://www.googleapis.com/youtube/v3/"
+
+//    let playlistURL = "\(BaseAPI.BASE_URL)playlists?part=snippet&\(BaseAPI.ID_USER)&\(BaseAPI.API_KEY)"
+
+}
--- Gigigo-YouTube/Controllers/UserPlaylistsViewController.swift
+++ Gigigo-YouTube/Controllers/UserPlaylistsViewController.swift
@@ -8,9 +8,35 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 class UserPlaylistsViewController: UITableViewController {
+
+    var playlists: [YoutubePlaylistItem?] = []
     
-    
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Playlists de Apple"
+        
+        let playlistURL = "\(BaseAPI.baseURL)playlists?part=snippet&\(BaseAPI.idUser)&\(BaseAPI.apiKey)"
+        Alamofire.request(playlistURL).responseObject { (response: DataResponse<PlaylistsList>) in
+            print(response)
+            guard let playlistData = response.result.value?.playlists else { return }
+            self.playlists = playlistData
+            self.tableView.reloadData()
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return playlists.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+        cell.textLabel?.text = playlists[indexPath.row]?.title
+        return cell
+    }
+
 }
