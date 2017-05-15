854d0af723638da31d9f7fedc49e4a197ec80fb9
Allan <allan.martins@resource.com.br>
2017-05-15 12:17:25 -0300

    implementing tests

--- Gigigo-YouTubeTests/Gigigo_YouTubeTests.swift
+++ Gigigo-YouTubeTests/Gigigo_YouTubeTests.swift
@@ -7,30 +7,142 @@
 //
 
 import XCTest
+import ObjectMapper
+import AlamofireObjectMapper
 @testable import Gigigo_YouTube
 
 class Gigigo_YouTubeTests: XCTestCase {
     
+    var viewController: UserPlaylistsViewController?
+    var navController: UINavigationController?
+    var storyBoard: UIStoryboard?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        self.storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        self.navController = self.storyBoard?.instantiateInitialViewController() as? UINavigationController
+        let _ = self.navController?.view
+        self.viewController = self.navController?.topViewController as? UserPlaylistsViewController
+        let _ = self.viewController!.view
+        
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testViewControllerExistence() {
+        XCTAssertNotNil(self.viewController, "view controller no instantiated")
+        XCTAssertNotNil(self.viewController?.tableView, "tableview not instntiated")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testTableView() {
+        
+        guard let playlists = self.mockPlaylists() else {
+            XCTAssertTrue(false, "unable to mock playlists")
+            return
         }
+        
+        guard let playlistData = playlists.playlists else {
+            XCTAssertTrue(false, "unable to mock playlists")
+            return
+        }
+        
+        self.viewController?.playlists = playlistData
+        self.viewController?.tableView.reloadData()
+        
+        XCTAssert((self.viewController?.tableView.numberOfRows(inSection: 0))! == 10)
+        
+        
+    }
+    
+    func testPlaylistItemCell() {
+        
+        guard let playlists = self.mockPlaylists() else { return }
+        guard let playlistData = playlists.playlists else { return }
+        self.viewController?.playlists = playlistData
+        self.viewController?.tableView.reloadData()
+        let cell = self.viewController?.tableView.cellForRow(at: IndexPath(row: 2, section: 0))
+            as! PlaylistItemCell
+        
+        XCTAssertTrue(cell.title.text == "Discover More", "cell didn't load correctly")
+        
+        self.viewController?.performSegue(withIdentifier: "playlistDetails", sender: cell)
+        let details = self.storyBoard?.instantiateViewController(withIdentifier: "PlaylistDetailsViewController")
+            as? PlaylistDetailsViewController
+        let _ = details!.view
+        
+        XCTAssertNotNil(details, "view controller no instantiated")
+        XCTAssertNotNil(details?.tableView, "tableview not instntiated")
+
+    }
+    
+    func testPlaylistDetails() {
+        
+        guard let playlists = self.mockPlaylists() else { return }
+        guard let playlistData = playlists.playlists else { return }
+        self.viewController?.playlists = playlistData
+        self.viewController?.tableView.reloadData()
+        let cell = self.viewController?.tableView.cellForRow(at: IndexPath(row: 2, section: 0))
+            as! PlaylistItemCell
+        
+        let details = self.storyBoard?.instantiateViewController(withIdentifier: "PlaylistDetailsViewController")
+            as? PlaylistDetailsViewController
+        let _ = details!.view
+        
+        let row = self.viewController?.tableView.indexPath(for: cell)!.row
+        let item = self.viewController?.playlists[row!]
+        details?.title = item?.title
+        details?.playlistID = (item?.playlistID)!
+        
+        guard let playlistVideos = self.mockPlaylistDetails()?.videos else { return }
+        details?.videos = playlistVideos
+        details?.tableView.reloadData()
+        
+        let detailsCell = details?.tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+            as! PlaylistItemDetailsCell
+        
+        XCTAssertTrue(detailsCell.title.text == "How to shoot stills while filming on iPhone 7 — Apple",
+                      "cell didn't load correctly")
+    }
+    
+    func mockPlaylists() -> PlaylistsList? {
+        
+        let path = Bundle(for: Gigigo_YouTubeTests.self)
+            .path(forResource: "playlists", ofType: "json")
+        let JSONData: Data?
+        
+        do {
+            
+            JSONData = try Data(contentsOf: URL(fileURLWithPath: path!))
+            let jsonResult: NSDictionary = try (JSONSerialization
+                .jsonObject(with: JSONData!, options: .mutableContainers) as? NSDictionary)!
+            
+            let playlists = PlaylistsList(JSON: jsonResult as! [String : Any])
+            return playlists ?? nil
+            
+        } catch { return nil }
+        
+    }
+    
+    func mockPlaylistDetails() -> YoutubePlaylist? {
+    
+        let path = Bundle(for: Gigigo_YouTubeTests.self)
+            .path(forResource: "playlist", ofType: "json")
+        let JSONData: Data?
+        
+        do {
+            
+            JSONData = try Data(contentsOf: URL(fileURLWithPath: path!))
+            let jsonResult: NSDictionary = try (JSONSerialization
+                .jsonObject(with: JSONData!, options: .mutableContainers) as? NSDictionary)!
+            
+            let playlists = YoutubePlaylist(JSON: jsonResult as! [String : Any])
+            return playlists ?? nil
+            
+        } catch { return nil }
+        
     }
     
 }
